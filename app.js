bafb12bb0541e56c9ba420ccfa90cf32b2cef6e8
Chadwick Green <chadggreen@gmail.com>
2021-04-17 13:11:10 -0400

    missile creation and event listener - incomplete

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ let playerIcon = document.getElementById('playerIcon');
 // PLAYER CREATION START------------------------------
 class Player {
     constructor(screenWidth, screenHeight){
-        this.width = 50;
-        this.height = 50;
+        this.width = 64;
+        this.height = 64;
 
         this.maxSpeed = 8;
         this.speed = 0;
@@ -63,10 +63,65 @@ player.drawPlayer(ctx)
 // PLAYER CREATION END--------------------------------
 
 
+// MISSILE CREATION START--------------------------------------
+class Missile {
+    constructor(){
+        this.width = 5;
+        this.height = 20;
+        this.position = {
+            x: player.position.x + 30,
+            y: player.position.y -20,
+        }
+        this.speed = 10
+    }
+    drawMissile(ctx){
+        ctx.fillStyle = '#F46036'
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+    update(deltaTime){
+        this.drawMissile(ctx)
+        if(!deltaTime){
+            return
+        }
+        // this.position.y -= this.speed/deltaTime
+        this.position.y -= this.speed;
+    }
+}
+let missile = new Missile()
+missile.drawMissile(ctx)
+
+
+
+// class Missile {
+//     constructor(){
+//         this.x = x;
+//         this.y = y;
+//         this.radius =radius;
+//         this.color = color;
+//         this.velocity = this.velocity;
+//     }
+//     drawMissile(ctx){
+//         ctx.beginPath()
+//         ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+//         ctx.fillStyle = this.color
+//         ctx.fill()
+//     }
+//     update(){
+//         this.x = this.x +this.velocity
+//         this.y = this.y + this.velocity
+//     }
+// }
+// let missile = new Missile()
+// missile.drawMissile(ctx)
+
+// MISSILE CREATION END--------------------------------------
+
 
 
 
 // INPUT HANDLERS START-------------------------
+
+// -----Player Inputs
 class InputHandler{
     constructor(player){
         // event listener for keydown event
@@ -106,6 +161,41 @@ class InputHandler{
     }
 }
 new InputHandler(player)
+
+// -----Missile Inputs
+class MissileInput {
+    constructor(missile){
+        document.addEventListener('keydown', event =>{
+            // alert(event.key)
+            switch(event.key){
+                case 'ArrowUp':
+                    missile.update(deltaTime);
+                    break;
+            }
+        })
+    }
+}
+new MissileInput(missile)
+
+
+
+
+// document.addEventListener('keydown', event =>{
+//     const missile = new Missile(
+//         player.position.x,
+//         player.position.y,
+//         5,
+//         'red',
+//         {
+//             x:0,
+//             y:-1,
+//         }
+//     )
+//     missile.drawMissile()
+//     missile.update()
+//     })
+
+// ***** input declarations have to be before gameLoop to work
 // INPUT HANDLERS END---------------------------
 
 
@@ -119,13 +209,15 @@ function gameLoop(timestamp){
     let deltaTime = timestamp - lastTime;
     lastTime = timestamp
 
-
     // this clears the screen each frame
     ctx.clearRect(0,0,screenWidth,screenHeight)
     // this updates the player position
     player.update(deltaTime)
     // this redraws the player
     player.drawPlayer(ctx)
+
+    missile.update(deltaTime)
+    // missile.drawMissile(ctx)
 
     // calls the gameloop again with the next frames timestamp
     requestAnimationFrame(gameLoop)
